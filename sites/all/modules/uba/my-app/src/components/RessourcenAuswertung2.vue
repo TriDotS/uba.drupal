38f55f99683e99fb33a1ac567b721597a3fb48d1
Aki <aki@Akis-MacBook-Pro.local>
2018-06-19 17:05:08 +0200

    Piwik Indikator test

--- sites/all/modules/uba/my-app/src/components/RessourcenAuswertung2.vue
+++ sites/all/modules/uba/my-app/src/components/RessourcenAuswertung2.vue
@@ -262,14 +262,8 @@
             },
 		    
             onSubmit() {
-                _paq.push(['trackSiteSearch',
-                    // Search keyword searched for
-                    "Banana",
-                    // Search category selected in your search engine. If you do not need this, set to false
-                    "Organic Food",
-                    // Number of results on the Search results page. Zero indicates a 'No Result Search Keyword'. Set to false if you don't know
-                    0
-                ]);
+                let _paq = _paq || [];
+                console.log(_paq);
                 this.updateTable();
             },
 
